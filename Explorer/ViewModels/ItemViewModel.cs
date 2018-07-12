b8bd621aa6cad29a5fef02177dac420e9355f482
Edgardo Zoppi <edgarzpp@gmail.com>
2018-07-12 01:02:27 -0700

    Updating code to use the unified type definition class.

--- Explorer/ViewModels/ItemViewModel.cs
+++ Explorer/ViewModels/ItemViewModel.cs
@@ -195,9 +195,9 @@ namespace Explorer
 
 	class TypeViewModel : ItemViewModelBase
 	{
-		private ITypeDefinition type;
+		private TypeDefinition type;
 
-		public TypeViewModel(MainViewModel main, ITypeDefinition type)
+		public TypeViewModel(MainViewModel main, TypeDefinition type)
 			: base(main)
 		{
 			this.type = type;
@@ -223,9 +223,9 @@ namespace Explorer
 		{
 			ItemViewModelBase result = null;
 
-			if (member is ITypeDefinition)
+			if (member is TypeDefinition)
 			{
-				var type = member as ITypeDefinition;
+				var type = member as TypeDefinition;
 				result = new TypeViewModel(this.Main, type);
 			}
 			else if (member is FieldDefinition)
